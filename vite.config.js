4f0f55db77020a33fe6201be02df9b3c0b66c10e
nj-ysf <najiy514@gmail.com>
2026-03-15 14:07:03 +0000

    add  some project to project section

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/nj-ysf.github.io/",   // doit correspondre au nom du repo GitHub
-  plugins: [react()],
-  server: {
-    port: 5173,
-    open: true,
-  },
-});
+  base: "/",
+  plugins: [react()]
+})
